7fbc6d122fcc188933f691a3e4f1c9f8661a85d4
aksenoff.a <aksenoff.a@8adfffa7-0b46-0410-a3b1-992bbaf3fd1d>
2008-03-26 14:18:26 +0000

    user registration added
    git-svn-id: https://aiweb.googlecode.com/svn/trunk@31 8adfffa7-0b46-0410-a3b1-992bbaf3fd1d

--- aiweb.py
+++ aiweb.py
@@ -1,28 +1,61 @@
 # coding: cp1251
 
 from pony.main import *
+from pony.thirdparty import sqlite
+from datetime import *
+import sha
 use_autoreload()
 
 import core
 
 users, messages = core.load_from_file()
 
+def connect():
+    return sqlite.connect('aiweb.db3')
+
 class LoginForm(Form):
     def __init__(self):
-        self.login = Text(u'Логин')
-        self.password = Text(u'Пароль')
-        self.button = Submit(u'Вход')
+        Form.__init__(self, method='POST')
+        self.grid = Grid(columns=['', ''])
+        self.grid.row_count = 2
+        self.grid[0, 0] = Text(u'Логин')
+        self.grid[0, 1] = Submit(u'Вход')
+        self.grid[1, 0] = Text(u'Пароль')
+        self.grid[1, 1] = None
 
-##    def __init__(self):
-##        l1 = self.add_layout(True)
-##        l2 = l1.add_layout(False)
-##        l2.login = Text(u'Логин')
-##        l2.password = Text(u'Пароль')
-##        l1.button = Submit(u'Вход')
+
+class RegForm(Form):
+    def __init__(self):
+        self.login=Text(u'Логин',required=True)
+        self.email = Text(u'Почтовый адрес')
+        self.password = Password(u'Пароль', required=True)
+        self.password2 = Password(u'Повтор пароля', required=True)
+        self.con = connect()
+    def validate(self):        
+        if self.password.value!=self.password2.value:
+            self.password.error_text = self.password2.error_text = u'Пароли не совпадают!'
+        row = self.con.execute(u'select id from Users where login = ?', [ self.login.value ]).fetchone()
+        if row is not None:
+            self.login.error_text = u'Такой логин уже занят'
+    def on_submit(self):
+        hash = sha.new(self.password.value).hexdigest()
+        cursor = self.con.execute(u'insert into Users(login, password, email, rating, disabled, reg_date) values(?, ?, ?, 1, 0, ?)',
+                             [ self.login.value, hash, self.email.value, datetime() ])
+        user_id = cursor.lastrowid
+        set_user(user_id)
+        get_session()['login'] = self.login.value
+        self.con.commit()
+        
 
 @printhtml
 def registration_component():
-    return LoginForm()
+    user_id = get_user()
+    if user_id is None:
+        return LoginForm()
+    con=connect()
+    login = con.execute(u'select login from Users where id=?',[user_id]).fetchone()
+    print u'<h3>Вы вошли как: %s</h3>' % (login)
+    print u'<p>%s</p>' % link(u'Выйти',logout)
     
 @http('/')
 @printhtml
@@ -31,6 +64,19 @@ def main():
     mranks = sorted(messages.values(), key=lambda x: x.rank, reverse=True)
     return html()
 
-@http('/user/')
+@http('/register')
+@printhtml
+def register():
+    f = RegForm()
+    print u"Заполните необходимые регистрационные данные:"
+    print f
+
+@http('/logout')
+@printhtml
+def logout():
+    user = get_session()['login']
+    set_user(None)
+    print html(u"""$if (user) { До встречи, $user!}
+                <h2>Вы вышли</h2>""")
 
 http.start()    
